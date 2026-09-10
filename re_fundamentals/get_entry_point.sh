662010efce46a15ad45749761f9b2d27c212bb6c
witcher <witcher@witcher.witcher>
2026-09-10 14:29:00 -0400

    reversing...

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <elf_file>"
-    exit 1
-fi
-
 file_name="$1"
 
 if [ ! -f "$file_name" ]; then
@@ -17,17 +12,11 @@ if ! readelf -h "$file_name" >/dev/null 2>&1; then
     exit 1
 fi
 
-magic_number=$(readelf -h "$file_name" | grep "Magic:" | sed 's/^[[:space:]]*Magic:[[:space:]]*//')
-class=$(readelf -h "$file_name" | grep "Class:" | sed 's/^[[:space:]]*Class:[[:space:]]*//')
-byte_order=$(readelf -h "$file_name" | grep "Data:" | sed 's/^[[:space:]]*Data:[[:space:]]*//' | sed 's/^[^,]*, //')
-entry_point_address=$(readelf -h "$file_name" | grep "Entry point address:" | sed 's/^[[:space:]]*Entry point address:[[:space:]]*//')
+magic_number=$(readelf -h "$file_name" | grep "Magic:" | xargs | cut -d' ' -f2-)
+class=$(readelf -h "$file_name" | grep "Class:" | xargs | cut -d' ' -f2-)
+byte_order=$(readelf -h "$file_name" | grep "Data:" | sed "s/.*2's complement, //")
+entry_point_address=$(readelf -h "$file_name" | grep "Entry point address:" | xargs | cut -d' ' -f4)
 
 source "$(dirname "$0")/messages.sh"
 
 display_elf_header_info
-
-
-
-
-
-
