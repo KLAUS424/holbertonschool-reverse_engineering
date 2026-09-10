cd2a8be4cd7ac2a0fd8e06802dcf0a7b3885d672
witcher <witcher@witcher.witcher>
2026-09-10 14:19:13 -0400

    reversing...

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -19,9 +19,15 @@ fi
 
 magic_number=$(readelf -h "$file_name" | grep "Magic:" | sed 's/^[[:space:]]*Magic:[[:space:]]*//')
 class=$(readelf -h "$file_name" | grep "Class:" | sed 's/^[[:space:]]*Class:[[:space:]]*//')
-byte_order=$(readelf -h "$file_name" | grep "Data:" | sed 's/^[[:space:]]*Data:[[:space:]]*//' | sed 's/ (.*//')
+byte_order=$(readelf -h "$file_name" | grep "Data:" | sed 's/^[[:space:]]*Data:[[:space:]]*//' | sed 's/^[^,]*, //')
 entry_point_address=$(readelf -h "$file_name" | grep "Entry point address:" | sed 's/^[[:space:]]*Entry point address:[[:space:]]*//')
 
-source ./messages.sh
+source "$(dirname "$0")/messages.sh"
 
 display_elf_header_info
+
+
+
+
+
+
